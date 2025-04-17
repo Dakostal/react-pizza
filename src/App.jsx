33f32573c1749cd8c9eb5d61fee9507da9bff9e3
Dakostal <kosievdanon@gmail.com>
2025-04-17 17:41:49 +0600

    useStatecomponent

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import './scss/app.scss'
 import { Header } from './components/header/Header'
 import { Categories } from './components/categories/Categories'
 import { Sort } from './components/sort/Sort'
-import { PizaaBlock } from './components/pizzaBlock/PizzaBlock'
+import { PizzaBlock } from './components/pizzaBlock/PizzaBlock'
 
 function App() {
 
   return (
     <>
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
           <Categories />  
           <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-          <PizaaBlock title="Мексиканская" price={345}/>
-          <PizaaBlock title="Сырная" price={320}/>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+          <PizzaBlock title="Мексиканская" price={345}/>
+          <PizzaBlock title="Сырная" price={320}/>
           </div>
         </div>
       </div>
--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,18 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
+
+const categories = [
+    {id: 1, name: 'Все', active: false},
+    {name: 'Мясные', active: false},
+    {name: 'Вегетарианская', active: false},
+    {name: 'Гриль', active: false},
+    {name: 'Острые', active: false},
+    {name: 'Закрытые', active: false}
+]
 
 export const Categories =()=> {
+
+    const[isActive, setIsActive] = useState(false)
+
+    const onClickCategories = () => {
+        setIsActive(isActive === false)
+    }
     return (
         <>
             <div className="categories">
                 <ul>
-                    <li className="active">Все</li>
-                    <li>Мясные</li>
-                    <li>Вегетарианская</li>
-                    <li>Гриль</li>
-                    <li>Острые</li>
-                    <li>Закрытые</li>
-                </ul>
-                </div>
+                    {categories.map((cat, i) => (
+                        <li
+                            onClick={onClickCategories}
+                            className={isActive ?  'active' : cat.active}
+                            key={i}
+                        >{cat.name}</li>
+                    ))}
+                </ul>    
+            </div>
         </>
     )
 }
