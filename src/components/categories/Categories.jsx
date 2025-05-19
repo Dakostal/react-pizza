0f0f5f8e9ccbe28c916b40af9d106cbdd79d0e56
Dakostal <kosievdanon@gmail.com>
2025-05-19 08:56:43 +0700

    refactoringFullCode

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,34 +1,31 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 
 const categories = [
-    {name: 'Все'},
-    {name: 'Мясные'},
-    {name: 'Вегетарианская'},
-    {name: 'Гриль'},
-    {name: 'Острые'},
-    {name: 'Закрытые'}
+    { name: 'Все' },
+    { name: 'Мясные' },
+    { name: 'Вегетарианская' },
+    { name: 'Гриль' },
+    { name: 'Острые' },
+    { name: 'Закрытые' }
 ]
 
-export const Categories =()=> {
-
-    const[isActive, setIsActive] = useState(0)
+export const Categories = () => {
+    const [isActive, setIsActive] = useState(0)
 
     const onClickCategories = (index) => {
         setIsActive(index)
-        console.log(setIsActive);
+        console.log(setIsActive)
     }
     return (
         <>
             <div className="categories">
                 <ul>
                     {categories.map((cat, i) => (
-                        <li
-                            onClick={()=>onClickCategories(i)}
-                            className={isActive === i ? 'active' : ''}
-                            key={i}
-                        >{cat.name}</li>
+                        <li onClick={() => onClickCategories(i)} className={isActive === i ? 'active' : ''} key={i}>
+                            {cat.name}
+                        </li>
                     ))}
-                </ul>    
+                </ul>
             </div>
         </>
     )
